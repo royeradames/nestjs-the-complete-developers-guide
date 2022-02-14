8143a7760891233190485be601817deeb390d0b3
Royer Adames <royeraadames@gmail.com>
2022-02-14 08:29:47 -0500

    ✅ export statements in typescript

--- design-patterns/src/User.ts
+++ design-patterns/src/User.ts
@@ -1,6 +1,12 @@
 /* file that handle import of classes get their first letter capitalize */
 
 import faker from "faker";
+
+/* export is the default in ts so that you always use  {} to import things, and
+    default export is avoid so you don't have to
+    - guest if you are assigning a new variable name to an import
+    - keep type object name
+ */
 export class User {
   name: string;
   location: {
