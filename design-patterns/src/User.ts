15261493567bef2194f5af1af307f242a5293788
Royer Adames <royeraadames@gmail.com>
2022-02-14 08:14:14 -0500

    ✅ generating random data

--- design-patterns/src/User.ts
+++ design-patterns/src/User.ts
@@ -1,1 +1,17 @@
 /* file that handle import of classes get their first letter capitalize */
+
+import faker from "faker";
+export class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+}
