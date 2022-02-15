ab7cbe0bf503bbbecffbaea54728c4a7951173a9
Royer Adames <royeraadames@gmail.com>
2022-02-14 19:51:49 -0500

    ✅ hiding functionality

--- design-patterns/src/index.ts
+++ design-patterns/src/index.ts
@@ -1,6 +1,7 @@
 // import { Company } from "./Company";
 // import { User } from "./User";
 
+/* Would be great to hide all of the methods that are not use so that it makes it hard to break the app. The other google Map methods are not  */
 new google.maps.Map(document.getElementById("map"), {
   zoom: 1,
   center: { lat: 0, lng: 0 },
