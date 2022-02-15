256bd810ec87e69bd492bd700afe7b510cc8fed8
Royer Adames <royeraadames@gmail.com>
2022-02-14 20:20:59 -0500

    ✅ adding markers

--- design-patterns/src/CustomMap.ts
+++ design-patterns/src/CustomMap.ts
@@ -1,3 +1,6 @@
+import { Company } from "./Company";
+import { User } from "./User";
+
 /* Private modifier
 Would be great to hide all of the methods that are not use so that it makes it hard to break the app. The other google Map methods are not  */
 export class CustomMap {
@@ -9,6 +12,26 @@ export class CustomMap {
       center: {
         lat: 0,
         lng: 0,
+      },
+    });
+  }
+
+  addUserMaker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
+    });
+  }
+
+  addCompanyMarker(company: Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lng,
       },
     });
   }
--- design-patterns/src/index.ts
+++ design-patterns/src/index.ts
@@ -1,6 +1,13 @@
-// import { Company } from "./Company";
+import { Company } from "./Company";
 // import { User } from "./User";
 
 import { CustomMap } from "./CustomMap";
+import { User } from "./User";
+
+const user = new User();
+const company = new Company();
 
 const customMap = new CustomMap("map");
+
+customMap.addUserMaker(user);
+customMap.addCompanyMarker(company);
