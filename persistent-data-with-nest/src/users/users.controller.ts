47d83caf63af3058bb876b836dce9d260a9f168f
Royer Adames <royeraadames@gmail.com>
2022-02-23 06:53:54 -0500

    ✅ creating and saving a user

--- persistent-data-with-nest/src/users/users.controller.ts
+++ persistent-data-with-nest/src/users/users.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private userService: UsersService) {}
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
-    return body;
+    return this.userService.create(body.email, body.password);
   }
 }
--- persistent-data-with-nest/src/users/users.service.ts
+++ persistent-data-with-nest/src/users/users.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  /* 
+  : Repository<User>
+  type is a repository that is going to handle users 
+
+  @InjectRepository(User)
+  aid to repository
+  tells repository system that we need User
+    Repository<User> is a generics
+    it doesn't play nicely with generics 
+    @InjectRepository(User) fixes this
+  */
+  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    /* see typeorm entity methods image */
+    const user = this.userRepo.create({ email, password });
+
+    return this.userRepo.save(user);
+  }
+}
