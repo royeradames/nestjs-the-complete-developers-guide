278837a15725133e0b715f2ad10bd30ab31a8f04
Royer Adames <royeraadames@gmail.com>
2022-02-23 06:25:16 -0500

    ✅ manual route testing

--- persistent-data-with-nest/src/main.ts
+++ persistent-data-with-nest/src/main.ts
@@ -6,7 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   /* allow validation pipe through the app */
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      /* remove additional properties that are not listed in the dto */
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
