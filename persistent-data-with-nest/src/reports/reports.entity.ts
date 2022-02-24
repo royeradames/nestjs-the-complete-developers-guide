f6b310837d3faa7995a06b2eaa1b53e5c422fc2e
Royer Adames <royeraadames@gmail.com>
2022-02-24 09:49:10 -0500

    ✅ a dto for report creation

--- persistent-data-with-nest/src/reports/reports.entity.ts
+++ persistent-data-with-nest/src/reports/reports.entity.ts
@@ -7,4 +7,22 @@ export class Reports {
 
   @Column()
   price: number;
+
+  @Column()
+  make: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: Date;
+
+  @Column()
+  mileage: number;
+
+  @Column()
+  longitude: number;
+
+  @Column()
+  latitude: number;
 }
